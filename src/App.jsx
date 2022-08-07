b5d2505035b7c1d6270e6529171f5be259f4c6d8
Claudio Alexis <irianniclaudio@gmail.com>
2022-08-07 02:29:30 -0300

    add onclick, onchange

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,29 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
 
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value)
+  }
+
+ onSubmit = (e) => {
+  console.log("click")
+ }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
         {/*<FaceRecognition /> */}
       </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactDOM from 'react-dom'
 import "./index.css";
 import "tachyons";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
